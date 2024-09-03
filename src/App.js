5670c48464532688ecc7d8620be5806a52854b96
Allahya Kwada <62298934+Alasco95@users.noreply.github.com>
2024-09-03 17:33:29 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,13 @@ const App = () => {
         SearchMovies('blacklist')
     }, []);
 
-    
+    const movie1 = {
+        "Title": "Spiderman the Verse",
+        "Year": "2019",
+        "imdbID": "tt12122034",
+        "Type": "series",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNjA2NmZhOGEtZTQ5OS00MDI0LTg4N2UtYTRmOTllM2I2NDlhXkEyXkFqcGdeQXVyNTU4OTE5Nzc@._V1_SX300.jpg"
+    }
 
     return (
         <div className ='app'>
